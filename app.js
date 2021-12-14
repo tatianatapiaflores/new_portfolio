f9826062e877b590bc8abd020f6dccef4e365d06
tatianatapiaflores <68444509+tatianatapiaflores@users.noreply.github.com>
2021-12-13 17:30:06 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const augie = document.querySelector('.augie__inner');
+
+augie.addEventListener('click', function () 
+{
+    augie.classList.toggle('is-flipped');
+});
+ 
+
+const olivia = document.querySelector('.olivia__inner');
+
+olivia.addEventListener('click', function () 
+{
+    olivia.classList.toggle('is-flipped');
+});
+
+
+const grace = document.querySelector('.grace__inner');
+
+grace.addEventListener('click', function () 
+{
+    grace.classList.toggle('is-flipped');
+});
